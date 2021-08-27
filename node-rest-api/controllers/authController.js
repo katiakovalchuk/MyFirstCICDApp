848abf239e22cc00af2ca1afbe212e325af4a40e
Kateryna_Kovalchuk <kate.kovalchuk225@gmail.com>
2021-08-27 15:49:42 +0300

    modified login

--- node-rest-api/controllers/authController.js
+++ node-rest-api/controllers/authController.js
@@ -6,6 +6,11 @@ router.post('/signin', async (req, res) => {
   try{
     const {email, password} = req.body;
     const token = await signIn({email, password});
+    // res.setHeader("Access-Control-Allow-Origin", "*");
+    // res.setHeader("Access-Control-Allow-Credentials", "true");
+    // res.setHeader("Access-Control-Max-Age", "1800");
+    // res.setHeader("Access-Control-Allow-Headers", "content-type");
+    // res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
     res.json({token, message: 'Logged in successfully!'})
   }catch (err){
     if (err){
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,7 +5,11 @@ import { Observable } from 'rxjs';
 const AUTH_API = 'http://localhost:8080/api/auth/';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    // 'Access-Control-Allow-Origin': '*',
+    // 'Access-Control-Allow-Credentials': 'true'
+    })
 };
 
 @Injectable({
@@ -16,6 +20,7 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string): Observable<any> {
+    // return this.http.post(AUTH_API + 'signin', {
     return this.http.post(AUTH_API + 'signin', {
       username,
       password
